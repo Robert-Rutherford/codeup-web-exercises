afb35f4e13449a2109daedcb0d4a2fcf6df02d9b
Robert Rutherford <robertl.rutherford2@gmail.com>
2019-10-03 11:30:33 -0500

    October 3 clean-up all code.

--- js/welcome_js.js
+++ js/welcome_js.js
@@ -1,22 +1,24 @@
-var randomImageArray = ["exercise-img-1.jpeg","exercise-img-2.jpeg","exercise-img-3.jpeg","exercise-img-4.jpeg"];
-var randomBack = ["bg_img4_pattern.jpg","backgroundimg2.jpeg"];
+var randomImageArray = ["exercise-img-1.jpeg", "exercise-img-2.jpeg", "exercise-img-3.jpeg", "exercise-img-4.jpeg"];
+var randomBack = ["bg_img4_pattern.jpg", "backgroundimg2.jpeg"];
+
 function getRandomImage(imgAr, path) {
     path = path || 'img/'; // default path here
-    var num = Math.floor( Math.random() * imgAr.length );
-    var img = imgAr[ num ];
+    var num = Math.floor(Math.random() * imgAr.length);
+    var img = imgAr[num];
     var imgStr = '<img src="' + path + img + '" alt = "" class="img-fixer">';
     document.write(imgStr);
     document.close();
 }
 
-function geRandBack(imgAr, path){
+function geRandBack(imgAr, path) {
     path = path || 'img/'; // default path here
-    var num = Math.floor( Math.random() * imgAr.length );
-    var img = imgAr[ num ];
+    var num = Math.floor(Math.random() * imgAr.length);
+    var img = imgAr[num];
     var imgStr = path + img;
     document.write(imgStr);
     document.close();
 }
+
 //
 // var coll = document.getElementsByClassName("collapsible");
 // var i;
