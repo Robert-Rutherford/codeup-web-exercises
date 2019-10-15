d5917e9cc9249e9d15ced70b60da7dc532515e1c
Robert Rutherford <robertl.rutherford2@gmail.com>
2019-10-15 15:42:30 -0500

    October 15 loops exercises.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -4,22 +4,19 @@ function breakSkip() {
     var promptNumber = parseInt(prompt("Enter an odd number between 1 and 50."));
     console.log("Number to skip: " + promptNumber);
     var oddNum = 0;
-    while (true) {
+    while (oddNum < 50) {
         oddNum++;
-        if (oddNum % 2 === 0) {
-            continue;
+        if (oddNum % 2 !== 0) {
+            if (oddNum === promptNumber) {
+                console.log("Yikes! Skipping number: " + oddNum);
+                continue;
+            }
+            console.log("Here is an odd number: " + oddNum);
         }
-        if (oddNum === promptNumber) {
-            console.log("Yikes! Skipping number: " + oddNum);
-            continue;
-        }
-        if (oddNum >= 50) {
-            break;
-        }
-        console.log("Here is an odd number: " + oddNum);
     }
-
 }
+
+breakSkip();
 
 
 
--- js/loops-class.js
+++ js/loops-class.js
@@ -34,22 +34,22 @@ var myQuestion;
 
 // 4. Refactor question 3 using a do-while loop.
 // What is the behavior this time?
-do{
-    myQuestion = confirm("Would you Like to exit?");
-}while (myQuestion === false);
+// do {
+//     myQuestion = confirm("Would you Like to exit?");
+// } while (myQuestion === false);
 
 // 5. Create a do-while loop that prompts the user for a color and continues
 // to prompt until the string passed is "blue"
-var rightColor;
-do {
-    rightColor = prompt("enter the color blue.");
-}while (rightColor !== "blue");
+// var rightColor;
+// do {
+//     rightColor = prompt("enter the color blue.");
+// } while (rightColor !== "blue");
 
 // 6. Refactor question 1 using a do-while loop.
-var z = 0;
-do {
-    console.log(z += 10);
-}while (z <100);
+// var z = 0;
+// do {
+//     console.log(z += 10);
+// } while (z < 100);
 
 /*
 * FOR LOOPS
@@ -59,9 +59,9 @@ var myArray = ["apples", "oranges", "bananas", "grapes", "pineapples"];
 
 // 7. Create a for loop that iterates over this array and logs out each
 // element to the console
-for (var k = 0; k < myArray.length; k++){
-    console.log(myArray[k]);
-}
+// for (var k = 0; k < myArray.length; k++) {
+//     console.log(myArray[k]);
+// }
 
 // 8. You are going to use a for loop to complete the FizzBuzz exercise.
 // Your program should count up to 100.
@@ -69,23 +69,34 @@ for (var k = 0; k < myArray.length; k++){
 // For numbers divisible by 5, log "Buzz"
 // If divisible by 3 and 5, log "FizzBuzz"
 // All other cases, just log the number.
-for (var l = 1; l <= 100; l++){
-    if (l % 3 === 0 && l % 5 === 0 ){
-        console.log(l + " FizzBuzz");
-    }else if (l % 3 === 0){
-        console.log(l+" Fizz");
-    }else if (l % 5 === 0) {
-        console.log(l+" Buzz");
-    }else{
-        console.log(l);
-    }
-}
+// for (var l = 1; l <= 100; l++) {
+//
+//     // var outPut = l;
+//     // if (l % 3 === 0 || l % 5 === 0) { outPut = "";
+//     // if (l % 3 === 0) {outPut += "Fizz";}
+//     // if (l % 5 === 0) {outPut += "Buzz";}}
+//     // console.log(outPut);
+//
+//     // if (l % 3 === 0 && l % 5 === 0 ){
+//     //     console.log("FizzBuzz");
+//     // }else if (l % 3 === 0){
+//     //     console.log("Fizz");
+//     // }else if (l % 5 === 0) {
+//     //     console.log("Buzz");
+//     // }else{
+//     //     console.log(l);
+//     // }
+//
+//     console.log((++l%3?'':'Fizz')+(l%5?'':'Buzz')||l);
+//
+// }
 
 
 // 9. Refactor question 1 using a for-loop
-for (var numInc = 10; numInc <=100;numInc = numInc +10){
-    console.log(numInc);
-}
+var numInc = 10;
+// for (;numInc <= 100; numInc += 10) {
+//     console.log(numInc);
+// }
 
 // 10. Write a for-loop
 
@@ -95,18 +106,18 @@ for (var numInc = 10; numInc <=100;numInc = numInc +10){
 */
 
 // 11. Refactor question 1: using a 'break', stop the counting once it hits 70.
-for (numInc = 10; numInc <=100;numInc = numInc +10){
+for (numInc = 10; numInc <= 100; numInc = numInc + 10) {
     console.log(numInc);
-    if (numInc === 70){
+    if (numInc === 70) {
         break;
     }
 }
 // 12. Refactor question 2: using a 'continue', skip numbers 50-26 and only
 // log 25 - 1.
 var fifty = 50;
-while (fifty >1){
+while (fifty > 1) {
     fifty--;
-    if (fifty > 25){
+    if (fifty > 25) {
         continue;
     }
     console.log(fifty);
