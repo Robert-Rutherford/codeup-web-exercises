dd75df37ec205e1949eafdf2e940705f4700f2ee
Robert Rutherford <robertl.rutherford2@gmail.com>
2019-11-06 09:54:53 -0600

    November 6 weather map project

--- js/weather_map.js
+++ js/weather_map.js
@@ -9,109 +9,128 @@
         center: [-98.4916, 29.4252]
     });
 
+    //clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, partly-cloudy-night
+    var weatherIcons = {
+        clearDay: "<i class=\"wi wi-day-sunny\"></i>",
+        clearNight: "<i class=\"wi wi-night-clear\"></i>",
+        rain: "<i class=\"wi wi-rain\"></i>",
+        snow: "<i class=\"wi wi-snow\"></i>",
+        sleet: "<i class=\"wi wi-sleet\"></i>",
+        wind: "<i class=\"wi wi-windy\"></i>",
+        fog: "<i class=\"wi wi-fog\"></i>",
+        cloudy: "<i class=\"wi wi-cloudy\"></i>",
+        partlyCloudyDay: "<i class=\"wi wi-day-cloudy\"></i>",
+        partlyCloudyNight: "<i class=\"wi wi-night-alt-cloudy\"></i>",
+        failToIdentify: "<i class=\"wi wi-meteor\"></i>"
+    };
+
+    function generateIcon(weatherTag) {
+        weatherIcons
+    }
 
 
     function getWeather() {
-        var darkSkyAPISanAntonio = $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/"+darkSkyKey+"/29.4241,-98.4936");
+        var darkSkyAPISanAntonio = $.get("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/29.4241,-98.4936");
 
         darkSkyAPISanAntonio.done(function (data) {
             console.log(data);
             //today
             var todayDate = new Date(data.currently.time * 1000);
-            $('#todayDate').html("<b>"+todayDate+"</b>");
+            $('#todayDate').html("<b>" + todayDate + "</b>");
             // console.log(todayDate);
-        //    temperature
+            //    temperature
             var todayHigh = data.daily.data[0].temperatureHigh;
             var todayLow = data.daily.data[0].temperatureLow;
             // var todayTemp = data.currently.temperature;
             // console.log(todayTemp);
-            $('#todayTemp').html(todayHigh+"<span>&deg;</span> / "+todayLow+"<span>&deg;</span>");
-        //    icon
+            $('#todayTemp').html(todayHigh + "<span>&deg;</span> / " + todayLow + "<span>&deg;</span>");
+            //    icon
 
-            var todayweatherTag ="empty";
-        //    weather
+            var todayweatherTag = "empty";
+            //    weather
             var todayWeather = data.currently.summary;
             // console.log(todayWeather);
-            $('#todayWeather').html("<b>"+todayweatherTag+": </b>"+todayWeather);
-        //humidity
+            $('#todayWeather').html("<b>" + todayweatherTag + ": </b>" + todayWeather);
+            //humidity
             var todayHumid = Math.floor(data.currently.humidity * 100);
             // console.log(todayHumid);
-            $('#todayHumidity').html("<b>Humidity: </b>"+todayHumid);
-        //    wind
+            $('#todayHumidity').html("<b>Humidity: </b>" + todayHumid);
+            //    wind
             var todayWind = data.currently.windSpeed;
             // console.log(todayWind);
-            $('#todayWind').html("<b>Wind: </b>"+ todayWind);
-        //    pressure
+            $('#todayWind').html("<b>Wind: </b>" + todayWind);
+            //    pressure
             var todayPressure = data.currently.pressure;
             // console.log(todayPressure);
-            $('#todayPressure').html('<b>Pressure: </b>'+ todayPressure);
+            $('#todayPressure').html('<b>Pressure: </b>' + todayPressure);
 
 
-        //    tomorrow
+            //    tomorrow
             var tomorrowDate = new Date(data.daily.data[1].time * 1000);
             // console.log(tomorrowDate);
-            $('#tomorrowDate').html("<b>"+tomorrowDate+"</b>");
+            $('#tomorrowDate').html("<b>" + tomorrowDate + "</b>");
 
-        //    Temperature
+            //    Temperature
             var tomorrowHigh = data.daily.data[1].temperatureHigh;
             var tomorrowLow = data.daily.data[1].temperatureLow;
-            $('#tomorrowTemp').html(tomorrowHigh+"<span>&deg;</span> / "+tomorrowLow+"<span>&deg;</span>");
+            $('#tomorrowTemp').html(tomorrowHigh + "<span>&deg;</span> / " + tomorrowLow + "<span>&deg;</span>");
 
-        //    icon
+            //    icon
 
             var tomWeatherTag = 'also empty';
-        //    weather
+            //    weather
             var tomorrowWeather = data.daily.data[1].summary;
-            $('#tomorrowWeather').html("<b>"+tomWeatherTag+": </b>"+tomorrowWeather);
+            $('#tomorrowWeather').html("<b>" + tomWeatherTag + ": </b>" + tomorrowWeather);
 
-        //    humidity
+            //    humidity
             var tomorrowHumidity = Math.floor(data.daily.data[1].humidity * 100);
-            $('#tomorrowHumidity').html("<b>Humidity: </b>"+tomorrowHumidity);
+            $('#tomorrowHumidity').html("<b>Humidity: </b>" + tomorrowHumidity);
 
-        //    wind
+            //    wind
             var tomorrowWind = data.daily.data[1].windSpeed;
-            $('#tomorrowWind').html("<b>Wind: </b>"+ tomorrowWind);
+            $('#tomorrowWind').html("<b>Wind: </b>" + tomorrowWind);
 
             //    pressure
             var tomorrowPressure = data.daily.data[1].pressure;
             // console.log(tomorrowPressure);
-            $('#tomorrowPressure').html('<b>Pressure: </b>'+ tomorrowPressure);
+            $('#tomorrowPressure').html('<b>Pressure: </b>' + tomorrowPressure);
 
-        //    day after tomorrow
+            //    day after tomorrow
             var dayAfterDate = new Date(data.daily.data[2].time * 1000);
             // console.log(dayAfterDate);
-            $('#dayAfterTomorrowDate').html("<b>"+dayAfterDate+"</b>");
+            $('#dayAfterTomorrowDate').html("<b>" + dayAfterDate + "</b>");
 
             //    Temperature
             var dayAfterHigh = data.daily.data[2].temperatureHigh;
             var dayAfterLow = data.daily.data[2].temperatureLow;
-            $('#dayAfterTemp').html(dayAfterHigh+"<span>&deg;</span> / "+dayAfterLow+"<span>&deg;</span>");
+            $('#dayAfterTemp').html(dayAfterHigh + "<span>&deg;</span> / " + dayAfterLow + "<span>&deg;</span>");
 
             //    icon
 
             var dayAfterTag = 'also empty';
             //    weather
             var dayAfterWeather = data.daily.data[2].summary;
-            $('#dayAfterWeather').html("<b>"+dayAfterTag+": </b>"+dayAfterWeather);
+            $('#dayAfterWeather').html("<b>" + dayAfterTag + ": </b>" + dayAfterWeather);
 
             //    humidity
             var dayAfterHumidity = Math.floor(data.daily.data[2].humidity * 100);
-            $('#dayAfterHumidity').html("<b>Humidity: </b>"+dayAfterHumidity);
+            $('#dayAfterHumidity').html("<b>Humidity: </b>" + dayAfterHumidity);
 
             //    wind
             var dayAfterWind = data.daily.data[2].windSpeed;
-            $('#dayAfterWind').html("<b>Wind: </b>"+ dayAfterWind);
+            $('#dayAfterWind').html("<b>Wind: </b>" + dayAfterWind);
 
             //    pressure
             var dayAfterPressure = data.daily.data[2].pressure;
             // console.log(dayAfterPressure);
-            $('#dayAfterPressure').html('<b>Pressure: </b>'+ dayAfterPressure);
+            $('#dayAfterPressure').html('<b>Pressure: </b>' + dayAfterPressure);
 
 
         }).fail(function () {
             alert("ERROR: Dark Sky API Failed.");
         });
     }
+
     getWeather();
 
 })();
