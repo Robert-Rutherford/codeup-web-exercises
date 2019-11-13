065eeb32a31eb12c913226962276941960df1fe1
Robert Rutherford <robertl.rutherford2@gmail.com>
2019-11-13 16:31:57 -0600

    November 13 promise exercise

--- js/promises.js
+++ js/promises.js
@@ -3,11 +3,7 @@
 function wait(time) {
     const promise = new Promise((resolve, reject) => {
         setTimeout(()=>{
-            if (Math.random() > 0.1) {
-                resolve();
-            }else {
-                reject();
-            }
+            resolve();
         },time);
     });
     return promise;
@@ -16,28 +12,41 @@ function wait(time) {
 // wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 // wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
+const lastCommit = (username) =>{
+    fetch(`https://api.github.com/users/${username}/events`,
+        {headers: {'Authorization': 'token '+ githubToken}})
+        .then(resp => resp.json()).then(data => {
+        console.log(data);
+        let latestcommit = data.find(event => event.type === "PushEvent");
+        // console.log(latestcommit);
+        return latestcommit;
+    }).then(event => event.created_at).then(date => console.log(date));
+};
 
-function getGithubUsernames() {
-    return fetch('https://api.github.com/users',{headers: {'Authorization': 'token '+ githubToken}})
-        .then(response => response.json())
-        .then(users => users.map(user => user.login));
-}
-
-
-getGithubUsernames().then((usernames) => {
-    usernames.forEach((username) => {
-        // do something with each username
-        console.log(username);
-        // let events = fetch('https://api.github.com/users/'+username+'/events/public', { method: 'get'})
-        //     .then(publicevents =>{
-        //         return publicevents.json();
-        //         // console.log(publicevents);
-        //     }).then(events => events.map(event => event.));
-        // console.log(events);
+console.log(lastCommit('Robert-Rutherford'));
 
 
-    });
-}).catch(error => console.error(error));
+// function getGithubUsernames() {
+//     return fetch('https://api.github.com/users',{headers: {'Authorization': 'token '+ githubToken}})
+//         .then(response => response.json())
+//         .then(users => users.map(user => user.login));
+// }
+//
+//
+// getGithubUsernames().then((usernames) => {
+//     usernames.forEach((username) => {
+//         // do something with each username
+//         console.log(username);
+//         // let events = fetch('https://api.github.com/users/'+username+'/events/public', { method: 'get'})
+//         //     .then(publicevents =>{
+//         //         return publicevents.json();
+//         //         // console.log(publicevents);
+//         //     }).then(events => events.map(event => event.));
+//         // console.log(events);
+//
+//
+//     });
+// }).catch(error => console.error(error));
 
 
 // GET /users/:username/events
